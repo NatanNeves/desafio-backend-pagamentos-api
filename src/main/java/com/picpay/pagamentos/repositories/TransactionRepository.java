0907c943cb51a466382179280225d39c50e27c8a
Natan Neves <dev.natanbarbosa@gmail.com>
2024-06-29 16:26:00 -0300

    feat: creating Transaction Repository

---
+++ src/main/java/com/picpay/pagamentos/repositories/TransactionRepository.java
@@ -0,0 +1,9 @@
+package com.picpay.pagamentos.repositories;
+
+import com.picpay.pagamentos.domain.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
+}
